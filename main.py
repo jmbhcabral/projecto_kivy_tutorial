2d7f75d3c21070904aa1c8c10b80d9778768a0b3
jmbhcabral <larbak.cstrike@gmail.com>
2023-11-08 11:12:20 +0000

    App Dev - kivy navigation

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from kivy.app import App
+from navigation_screen_manager import NavigationScreenManager
+from kivy.properties import ObjectProperty
 
 
-# class GridLayoutExample(GridLayout):
-#     pass
+class MyScreenManager(NavigationScreenManager):
+    pass
 
 
 class ExtremeWayApp(App):
-    pass
+    manager = ObjectProperty(None)
+
+    def build(self):
+        self.manager = MyScreenManager()
+        return self.manager
 
 
 ExtremeWayApp().run()
